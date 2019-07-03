24f5225c35cd0eecd3577339f845464e5e84cc98
openvela-robot <openvela-robot@xiaomi.com>
2019-07-03 08:28:29 +0800

    Merge branch 'doc-fix' into 'master'
    doc: replace dbus-send's --address with --peer and --bus
    
    See merge request dbus/dbus!115

--- dbus/dbus-message-internal.h
+++ dbus/dbus-message-internal.h
@@ -143,6 +143,10 @@ _dbus_clear_variant (DBusVariant **variant_p)
   _dbus_clear_pointer_impl (DBusVariant, variant_p, _dbus_variant_free);
 }
 
+typedef struct DBusInitialFDs DBusInitialFDs;
+DBusInitialFDs *_dbus_check_fdleaks_enter (void);
+void            _dbus_check_fdleaks_leave (DBusInitialFDs *fds);
+
 DBUS_END_DECLS
 
 #endif /* DBUS_MESSAGE_INTERNAL_H */
